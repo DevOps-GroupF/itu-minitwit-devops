16d4bfc0e531a087c329e7ac2da6f1a044f364e6
Markus Schlögl <mlsc@itu.dk>
2024-04-21 14:32:12 +0200

    added fluentd for logging

--- MiniTwit/Program.cs
+++ MiniTwit/Program.cs
@@ -14,7 +14,7 @@ using OpenTelemetry.Metrics;
 using Prometheus;
 using Serilog;
 using Serilog.Exceptions;
-using Serilog.Sinks.Elasticsearch;
+using Serilog.Sinks.Fluentd;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -168,23 +168,8 @@ void configureLogging()
         .Enrich.WithExceptionDetails()
         .WriteTo.Debug()
         .WriteTo.Console()
-        .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+        .WriteTo.Fluentd("localhost", 24224, tag: "minitwit_dotnet")
         .Enrich.WithProperty("Environment", environment)
         .ReadFrom.Configuration(configuration)
         .CreateLogger();
-}
-
-static ElasticsearchSinkOptions ConfigureElasticSink(
-    IConfigurationRoot configuration,
-    string environment
-)
-{
-    return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
-    {
-        AutoRegisterTemplate = true,
-        IndexFormat =
-            $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment.ToLower()}-{DateTime.UtcNow:yyyy-MM}",
-        NumberOfReplicas = 1,
-        NumberOfShards = 2
-    };
 }
