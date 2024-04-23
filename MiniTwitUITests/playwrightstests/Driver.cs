fc76bdf8f0215788528dda22500cf9e2b68fc9a9
P. Rouvillain <17237293+prouv@users.noreply.github.com>
2024-04-23 17:19:28 +0200

    extra linting

--- MiniTwitUITests/playwrightstests/Driver.cs
+++ MiniTwitUITests/playwrightstests/Driver.cs
@@ -1,6 +1,6 @@
 using Microsoft.Playwright;
 
-namespace MiniTwitUITests.playwrightstests; 
+namespace MiniTwitUITests.playwrightstests;
 
 public class Driver : IDisposable
 {
@@ -18,15 +18,17 @@ public class Driver : IDisposable
         _requestContext?.Dispose();
     }
 
-
     private async Task<IAPIRequestContext?> CreateApiContext()
     {
         var playwright = await Playwright.CreateAsync();
 
-        return await playwright.APIRequest.NewContextAsync(new APIRequestNewContextOptions
-        {
-            BaseURL = "http://minitwit-service:8080/api/",
-            IgnoreHTTPSErrors = true
-        });
+        return await playwright.APIRequest.NewContextAsync(
+            new APIRequestNewContextOptions
+            {
+                BaseURL = "http://minitwit-service:8080/api/",
+                IgnoreHTTPSErrors = true
+            }
+        );
     }
 }
+
