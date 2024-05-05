d509912a911347ff96e8364da5eb01b0261c2124
Markus Schlögl <mlsc@itu.dk>
2024-05-05 15:47:55 +0200

    added cypress actions workflow

--- MiniTwit/cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'http://minitwit-service:8080/',
-    supportFile: false,
-    experimentalRunAllSpecs: true
-  },
-})
